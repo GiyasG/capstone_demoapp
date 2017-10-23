22f1a21f2e80668f1f8b786502cdfc534ca6f5c5
Giyas Guliyev <g.g.giyas@gmail.com>
2017-10-23 14:25:00 +0300

    Adding thinning tests

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -22,12 +22,31 @@ describe Foo, type: :model do
       # end
 
 
-      it "created Foo will be persistend, have a name, and be Found" do
-        expect(foo).to be_persisted
-        expect(foo.name).to eq("test")
-        expect(Foo.find(foo.id)).to_not be_nil
+      # it "created Foo will be persistend" do
+      #   expect(foo).to be_persisted
+      #   # binding.pry
+      # end
+      # it "created Foo will have a name" do
+      #   expect(foo.name).to eq("test")
+      #   # binding.pry
+      # end
+      # it "created Foo will be Found" do
+      #   expect(Foo.find(foo.id)).to_not be_nil
+      #   # binding.pry
+      # end
+
+      it {expect(foo).to be_persisted}
+      it {expect(foo.name).to eq("test")}
+      it {expect(Foo.find(foo.id)).to_not be_nil}
         # binding.pry
       end
 
-    end
+      context "created Foo (subject)" do
+      subject {Foo.create(:name=>"test")}
+      it {is_expected.to be_persisted}
+      it {expect(subject.name).to eq("test")}
+      it {expect(Foo.find(subject.id)).to_not be_nil}
+        # binding.pry
+      end
+
 end
