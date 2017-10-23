f2de25dfd517bfc4d063a1793710fce4e4a115ce
Giyas Guliyev <g.g.giyas@gmail.com>
2017-10-23 17:55:45 +0300

    Adding DatabaseCleaner ActiveRecord

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -2,7 +2,21 @@ require 'rails_helper'
 
 describe Foo, type: :model do
 
-    context "created Foo" do
+    before(:all) do
+      DatabaseCleaner[:active_record].strategy=:transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+    after(:all) do
+      DatabaseCleaner.clean_with(:truncation)
+    end
+    before(:each) do
+      DatabaseCleaner.start
+    end
+    after(:each) do
+      DatabaseCleaner.clean
+    end
+
+    context "created Foo (let)" do
       let(:foo) {Foo.create(:name=>"test")}
       # before(:all) do
       #   @foo=Foo.create(:name=>"test")
