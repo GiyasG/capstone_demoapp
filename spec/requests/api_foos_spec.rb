d1314fe9fa164b497b116c57283f2fc1971a68f4
Giyas Guliyev <g.g.giyas@gmail.com>
2017-10-24 17:34:41 +0300

    adding API CRUD tests, helper module

--- spec/requests/api_foos_spec.rb
+++ spec/requests/api_foos_spec.rb
@@ -4,10 +4,22 @@ RSpec.describe "Foo API", type: :request do
 
   include_context "db_cleanup_each", :transaction
 
-  describe "GET /api_foos" do
-    it "works! (now write some real specs)" do
-      get foos_path
-      expect(response).to have_http_status(200)
-    end
+  context "caller requests list of Foos" do
+    let!(:foos) { (1..5).map {|idx| FactoryGirl.create(:foo)} }
+
+  it "returns all instances" do
+    get foos_path, {:sample1=>"param", :sample2=>"param"},  #query string
+                   {"Accept"=>"application/json"}           #http headers
+    expect(request.method).to eq("GET")
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq("application/json")
+    expect(response["X-Frame-Options"]).to eq("SAMEORIGIN")
+
+    # payload=JSON.parse(response.body)
+    payload=parsed_body
+    expect(payload.count).to eq(foos.count)
+    expect(payload.map { |f| f["name"] }).to eq(foos.map { |f| f[:name] })
   end
+  end
+
 end
